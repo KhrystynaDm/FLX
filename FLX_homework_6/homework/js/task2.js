a83c74140165a8a44f18bb70641eab14eb3d319f
Khrystyna Okhota <xrustylichka@ukr.net>
2019-02-05 20:24:00 +0000

    JS change alert

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,10 +1,18 @@
 let price = Number(prompt('Enter amount of money (0 to 9999999)'));
 let discount = Number(prompt('Enter the discount (0 to 99 )'));
-let afterDiscount;
+let afterDiscount, saved;
 
 if ((price <= 9999999) && (price > 0) && (discount <= 99) && (discount > 0)) {
-    afterDiscount = price - (price * (discount / 100))
-    alert(parseFloat(afterDiscount.toFixed(2)));
-} else {
+    afterDiscount = price - (price * (discount / 100));
+    saved = price - afterDiscount;
+    alert(
+        'Price without discount:' + price + '\n' +
+        'Discount:' + parseFloat(discount.toFixed()) + '%' + '\n' +
+        'Price with discount:' + parseFloat(afterDiscount.toFixed(2)) + '\n' +
+        'Saved:' + parseFloat(saved.toFixed(2))
+    );
+
+}
+else {
     alert('Invalid input data');
 }
