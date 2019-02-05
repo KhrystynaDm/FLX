3152230d0f57ad34d2dd6e7a4246cc05819068af
Khrystyna Okhota <xrustylichka@ukr.net>
2019-02-05 20:01:00 +0000

    JS add home work

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,5 +1,23 @@
-// Your code goes here
-function mylt(a,b, c){
-    return a * c * b;
+let a = Number(prompt("Please enter value a")),
+    b = Number(prompt("Please enter value b")),
+    c = Number(prompt("Please enter value c")), resultX, resultX1, resultX2;
+
+
+if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+    const discriminant = Math.pow(b, 2) - (4 * a * c);
+    const sqrDiscriminant = Math.sqrt(discriminant);
+    if (discriminant < 0) {
+        alert('No solution');
+    } else if (discriminant === 0) {
+        resultX = ((-b - sqrDiscriminant) / (2 * a));
+        alert("x1,x2: " + resultX);
+    } else if (discriminant > 0) {
+        resultX1 = ((-b + sqrDiscriminant) / (2 * a));
+        resultX2 = ((-b - sqrDiscriminant) / (2 * a));
+        alert("x1:" + resultX1 + " x2:" + resultX2);
+    }
+} else {
+    alert('Invalid input data');
 }
-mylt(2,5, 6);
+
+
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,1 +1,10 @@
-// Your code goes here
+let price = Number(prompt('Enter amount of money (0 to 9999999)'));
+let discount = Number(prompt('Enter the discount (0 to 99 )'));
+let afterDiscount;
+
+if ((price <= 9999999) && (price > 0) && (discount <= 99) && (discount > 0)) {
+    afterDiscount = price - (price * (discount / 100))
+    alert(parseFloat(afterDiscount.toFixed(2)));
+} else {
+    alert('Invalid input data');
+}
