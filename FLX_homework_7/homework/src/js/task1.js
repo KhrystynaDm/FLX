d040550c265eb66801eb790f10694efd04691509
Khrystyna Okhota <xrustylichka@ukr.net>
2019-02-09 15:57:35 +0000

    JS home work 7

--- FLX_homework_7/homework/src/js/task1.js
+++ FLX_homework_7/homework/src/js/task1.js
@@ -1,1 +1,33 @@
-// Your code goes here
+let visitirs = {
+    "User": "UserPass",
+    "Admin": "RootPass"
+}
+
+let currentHours = new Date().getHours();
+
+let login = prompt("Please enter login");
+
+if (!login || login === null) {
+    alert("Canceled.");
+} else if (login.length < 4) {
+    alert("I don't know any users having name length less than 4 symbols");
+} else if (visitirs[login]) {
+
+    let pass = prompt("Please enter pass");
+
+    if (!pass && pass === null) {
+        alert("Canceled.");
+    } else {
+        if (pass === "UserPass") {
+            currentHours < 20 ? alert("Good day, dear User!") : alert("Good evening, dear User!");
+
+        } else if (pass === "RootPass") {
+            currentHours < 20 ? alert("Good day, dear Admin!") : alert("Good evening, dear Admin!");
+
+        } else {
+            alert("Wrong password");
+        }
+    }
+} else {
+    alert("I don’t know you");
+}
--- FLX_homework_7/homework/src/js/task2.js
+++ FLX_homework_7/homework/src/js/task2.js
@@ -1,1 +1,76 @@
-// Your code goes here
+let doYouPlay = confirm("Do you want to play a game?"),
+    turns = 3,
+    x = 2 /*Math.floor(Math.random() * 6)*/,
+    minNumber = 0,
+    maxNumber = 5,
+    totalPrize = 0,
+    maxPossiblePrize = 10;
+if (doYouPlay === false) {
+    alert('You did not become a millionaire, but can.');
+} else {
+    while (turns > 0) {
+        let guessNum = prompt(
+            "Enter a number from " + minNumber + " to " + maxNumber + "\n" +
+            "Attemts left: " + turns + "\n" +
+            "Total prize: " + totalPrize + "$\n" +
+            "Possible prize in current attempt: " + maxPossiblePrize + "$");
+        if (parseInt(guessNum) === x) {
+            totalPrize += maxPossiblePrize;
+            alert("Congratulation! Your prize is:" + totalPrize + " $");
+            doYouPlay = confirm("Do you want to continue?");
+            if (doYouPlay === false) {
+                alert('Thank you for a game. Your prize is: ' + totalPrize + "$");
+                break;
+            } else {
+                turns = 3,
+                    x = 5 /*Math.floor(Math.random() * 11)*/,
+                    minNumber = 0,
+                    maxNumber = 10,
+                    maxPossiblePrize = 30;
+                while (turns > 0) {
+                    let guessNum = prompt(
+                        "Enter a number from " + minNumber + " to " + maxNumber + "\n" +
+                        "Attemts left: " + turns + "\n" +
+                        "Total prize: " + totalPrize + "$\n" +
+                        "Possible prize in current attempt: " + maxPossiblePrize + "$");
+                    if (parseInt(guessNum) === x) {
+                        totalPrize += maxPossiblePrize;
+                        alert("Congratulation! Your prize is:" + totalPrize + " $");
+                        doYouPlay = confirm("Do you want to continue?");
+
+                        if (doYouPlay === false) {
+                            alert('Thank you for a game. Your prize is: ' + totalPrize + "$");
+                            break;
+                        } else {
+                            break;
+                        }
+                    } else {
+                        alert('Thank you for a game. Your prize is: ' + totalPrize + "$");
+                        doYouPlay = confirm("Do you want to play a game again?");
+                        maxPossiblePrize = parseInt(maxPossiblePrize / 2);
+                        if (doYouPlay === false) {
+                            alert('You did not become a millionaire, but can.');
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                turns = turns - 1;
+            }
+        } else {
+            alert('Thank you for a game. Your prize is: ' + totalPrize + "$");
+            doYouPlay = confirm("Do you want to play a game again?");
+            maxPossiblePrize = parseInt(maxPossiblePrize / 2);
+
+            if (doYouPlay === false) {
+                alert('You did not become a millionaire, but can.');
+                break;
+            } else {
+                alert("test");
+            }
+            turns = turns - 1;
+        }
+
+    }
+}
