b956d512d0c56817cacfe423465989d7238ff6a6
Khrystyna Okhota <xrustylichka@ukr.net>
2019-02-10 20:01:18 +0000

    JS fixed function formatTime

---
+++ FLX_homework_8/homework/js/formatTime.js
@@ -0,0 +1,18 @@
+function formatTime(time) {
+    let timeNum = time;
+    let result = {};
+    let structure = {
+        day: 3600,
+        hour: 60,
+        minute: 1
+    };
+    Object.keys(structure).forEach(function (key) {
+        result[key] = Math.floor(timeNum / structure[key]);
+        timeNum -= result[key] * structure[key];
+    });
+    return `${result.day} day(s)  ${result.hour} hour(s) ${result.minute} minute(s)`;
+}
+
+formatTime(122);
+formatTime(3601); 
+formatTime(59);
